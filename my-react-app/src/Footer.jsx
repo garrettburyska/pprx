bb08f576d9c959239a37c4c6f59cde2a8ad4ddcc
garrettburyska <gburyska@protonmail.com>
2025-09-14 17:22:12 -0500

    hi

--- my-react-app/src/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer = () => (
-    <footer className="bg-black text-white text-center py-4 left-0 bottom-0 w-full">
-        <p>&copy; {new Date().getFullYear()} PX. All rights reserved.</p>
-    </footer>
-);
-
-export default Footer;
